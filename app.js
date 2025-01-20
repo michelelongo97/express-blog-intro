bd37fd4b8d71fcad6cb96bfff8cf968801101960
michelelongo97 <m.longo7x@gmail.com>
2025-01-20 19:26:30 +0100

    feat: add /articles

--- app.js
+++ app.js
@@ -8,6 +8,46 @@ app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
 
+app.get('/articles', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            title: "Il mio primo post",
+            content: "Questo è il contenuto del mio primo post.",
+            image: "/images/ciambellone.jpeg",
+            tags: ["Introduzione", "Primo post"]
+          },
+          {
+            id: 2,
+            title: "Il mio secondo post",
+            content: "Questo è il contenuto del mio secondo post.",
+            image: "/images/cracker_barbabietola.jpeg",
+            tags: ["Aggiornamento", "Secondo post"]
+          },
+          {
+            id: 3,
+            title: "Un altro post interessante",
+            content: "Qui parlo di cose interessanti.",
+            image: "/images/pane_fritto_dolce.jpeg",
+            tags: ["Tecnologia", "Innovazione"]
+          },
+          {
+            id: 4,
+            title: "Blogging per principianti",
+            content: "Come iniziare un blog personale.",
+            image: "/images/pasta_barbabietola.jpg",
+            tags: ["Blogging", "Tutorial"]
+          },
+          {
+            id: 5,
+            title: "Ultimo post del mese",
+            content: "Una panoramica di quanto successo questo mese.",
+            image: "/images/torta_paesana.jpeg",
+            tags: ["Recap", "Mese"]
+          }
+    ])
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
