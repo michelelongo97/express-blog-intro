bde8bea0d30b7c3868a5f51b25e7cd952278562d
michelelongo97 <m.longo7x@gmail.com>
2025-01-20 19:10:45 +0100

    init project

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+   
